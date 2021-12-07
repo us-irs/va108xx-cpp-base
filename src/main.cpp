d570dddb4923f5ff53ca0bf157797229ec73cea3
Robin Mueller <robin.mueller.m@gmail.com>
2021-12-07 10:52:00 +0100

    added some code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+#include "segger_rtt.h"
+#include "VORConfig.h"
 
+#include <cstdio>
+#include <cstddef>
 
-int main(void) {}
+int main(void) {
+#ifdef VOR_RTT_LOG
+    // Initialize Segger RTT
+    SEGGER_RTT_Init();
+    SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_TRIM);
+#endif
+
+    printf("Hello World!\n");
+    for(;;) {}
+}
